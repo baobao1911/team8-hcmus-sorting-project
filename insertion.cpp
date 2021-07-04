cadb13e5939f454bd528d4d385aed712b9165c8c
ntb1911 <79504672+baobao1911@users.noreply.github.com>
2021-07-04 19:47:14 +0700

    Add files via upload

---
+++ insertion.cpp
@@ -0,0 +1,13 @@
+﻿#include "sort.h"
+
+void InsertionSort(int a[], int n) {
+		int i = 0, j = 0, key = 0;
+		for (int i = 1;i < n; i++) {
+			key = a[i];
+			j = i - 1;
+			while (j >= 0 && a[j] > key) {
+				a[j + 1] = a[j];
+				j = j - 1;
+			}a[j + 1] = key;
+		}
+}
---
+++ quick.cpp
@@ -0,0 +1,26 @@
+﻿#include "sort.h"
+
+int Partition(int a[], int left, int right) {
+
+	int pivot = a[right];
+	int i, j;
+	i = (left - 1);
+	for (j = left; j <= right - 1; j++){
+		if (a[j] < pivot){
+			i++;
+			std::swap(a[i], a[j]);
+		}
+	}
+	std::swap(a[i + 1], a[right]);
+	return (i + 1);
+}
+
+void QuickSort(int a[], int left, int right) {
+	if (left < right)
+	{
+		int pi = Partition(a, left, right);
+
+		QuickSort(a, left, pi - 1);  // Before pi
+		QuickSort(a, pi + 1, right); // After pi
+	}
+}
