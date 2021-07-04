c273303c362903fa319a9bb99a891559b4aca22a
Lê Trần Trung Hiếu <trunghieumickey@outlook.com.vn>
2021-07-04 08:29:36 +0700

    Improve Heap Sort

--- heap.cpp
+++ heap.cpp
@@ -1,23 +1,22 @@
 #include "sort.h"
 
-void heatify(int a[], int n, int i)
+void HeapShift(int a[], int n, int i)
 {
     int largest = i;
     int l = 2 * i + 1;
-    int r = 2 * i + 2;
     if (l < n && a[l] > a[largest]) largest = l;
-    if (r < n && a[r] > a[largest]) largest = r;
+    if (l+1 < n && a[l+1] > a[largest]) largest = l+1;
     if (largest != i) { 
         swap(a[i], a[largest]);
-        heatify(a, n, largest);
+        HeapShift(a, n, largest);
     }
 }
 
 void HeapSort(int a[], int n)
 {
-    for (int i = n / 2 - 1; i >= 0; i--) heatify(a, n, i);
-    for (int i = n - 1; i > 0; i--) {
+    for (int i=n/2-1; i >= 0; i--) HeapShift(a, n, i);
+    for (int i=n-1; i > 0; i--) {
         swap(a[0], a[i]);
-        heatify(a, i, 0);
+        HeapShift(a, i, 0);
     }
 }
